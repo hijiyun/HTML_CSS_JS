44be82582f6eb103a7168b0b1665929734e0aa4b
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-01-26 23:04:29 +0900

    메뉴 underline완벽 정리 !!

--- Menu/main.js
+++ Menu/main.js
@@ -1,7 +1,7 @@
 let horizontalUnderline = document.getElementById("horizontal-underline"); 
-let horizontalMenus = document.getElementById("nav:first-child a");
-
-
+let verticalUnderline = document.getElementById("vertical-underline")
+let horizontalMenus = document.querySelectorAll("nav:first-child a");
+let verticalMenus = document.querySelectorAll("nav:nth-child(2) a");
 
 horizontalMenus.forEach((menu) => {
     menu.addEventListener("click", (e) => horizontalIndicator(e));
@@ -11,5 +11,15 @@ function horizontalIndicator(e) {
     horizontalUnderline.style.left = e.currentTarget.offsetLeft + "px";
     horizontalUnderline.style.width = e.currentTarget.offsetWidth + "px";
     horizontalUnderline.style.top =
+        e.currentTarget.offsetTop + e.currentTarget.offsetHeight + "px";
+}
+
+verticalMenus.forEach((menu) => {
+    menu.addEventListener("click", (e) => verticalIndicator(e));
+});
+function verticalIndicator(e) {
+    verticalUnderline.style.left = e.currentTarget.offsetLeft + "px";
+    verticalUnderline.style.width = e.currentTarget.offsetWidth + "px";
+    verticalUnderline.style.top =
         e.currentTarget.offsetTop + e.currentTarget.offsetHeight + "px";
 }
