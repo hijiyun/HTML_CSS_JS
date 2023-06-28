51335ab7c3998307561ed29da56d45896191568d
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-06-29 01:10:17 +0900

    id대로 콘솔에 나오고 화면 출력 js update

--- TodoList/main.js
+++ TodoList/main.js
@@ -1,7 +1,17 @@
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div")
 let taskList = []
+let filterList = []
+let mode = "all";
 addButton.addEventListener("click",addTask)
+
+for(let i=1; i<tabs.length; i++){
+    tabs[i].addEventListener("click", function(event){
+        filter(event)
+    });
+}
+
 
 //네모박스 안에 있는 value값과 끝났는지 안끝났는지 확인
 function addTask(){
@@ -18,21 +28,27 @@ function addTask(){
 //할일 추가된 task내용들을 ui로 보여주기 
 function render(){
     let resultHTML = '';
-    for(i = 0; i<taskList.length; i++){
-        if(taskList[i].isComplete == true){
+    let list = []
+    if(mode == "all"){
+        list = taskList
+    }else if(mode == "ongoing" || mode == "done"){
+        list = filterList
+    }
+    for(i = 0; i<list.length; i++){
+        if(list[i].isComplete == true){
             resultHTML += `<div class="task">
-                                <div class="constent task-done">${taskList[i].taskContent}</div>
+                                <div class="content task-done">${list[i].taskContent}</div>
                                 <div class="button-box">
-                                    <button class="check-btn" onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-rotate-left" style="color: #28529a;"></i></button>
-                                    <button class="delete-btn" onclick="deleteTask()"><i class="fa-solid fa-trash" style="color: #e12d2d;"></i></button>
+                                    <button class="check-btn" onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-rotate-left" style="color: #28529a;"></i></button>
+                                    <button class="delete-btn" onclick="deleteTask('${list[i].id}')"><i class="fa-solid fa-trash" style="color: #e12d2d;"></i></button>
                                 </div>
                         </div>`
         }else{
                 resultHTML += `<div class="task">
-                                    <div class="content">${taskList[i].taskContent}</div>
+                                    <div class="content">${list[i].taskContent}</div>
                                     <div class="button-box">
-                                        <button class="check-btn" onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-check" style="color: #247a26;"></i></button>
-                                        <button class="delete-btn" onclick="deleteTask()"><i class="fa-solid fa-trash" style="color: #e12d2d;"></i></button>
+                                        <button class="check-btn" onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-check" style="color: #247a26;"></i></button>
+                                        <button class="delete-btn" onclick="deleteTask('${list[i].id}')"><i class="fa-solid fa-trash" style="color: #e12d2d;"></i></button>
                                     </div>
                                 </div>`
             }
@@ -59,6 +75,32 @@ function randomIdGenerate(){
     return '_' + Math.random().toString(36).substr(2, 9);
 }
 
-function deleteTask(){
-    console.log("삭제")
+function deleteTask(id){
+    for(let i = 0; i<taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList.splice(i, 1)
+            break;
+        }
+    }
+    render();
+}
+
+function filter(event){
+    mode = event.target.id
+    let filterList = []
+    if(mode == "ongoing"){
+        for(let i=0; i<taskList.length; i++){
+            if(taskList[i].isComplete == false){
+                filterList.push(taskList[i])
+            }
+        }
+    }else if(mode == "done"){
+        for(let i=0;i<taskList.length; i++){
+            if(taskList[i].isComplete == true){
+                filterList.push(taskList[i])
+            }
+        }
+    }
+    console.log(filterList)
+    render();
 }
